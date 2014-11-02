3101bda30a336806eb2a32c3de8c5357c55d13b1
Fred Chien <cfsghost@gmail.com>
2014-11-02 14:34:08 +0800

    remove unneccessary code

--- tests/qmlloader/app.js
+++ tests/qmlloader/app.js
@@ -19,17 +19,3 @@ brig.on('ready', function(brig) {
 
 	});
 });
-/*
-// Create Engine
-var engine = new brig.QmlEngine();
-var rootContext = engine.rootContext();
-var context = new brig.QmlContext(rootContext);
-
-// Loading QML file
-var component = new brig.QmlComponent(engine, 'application.qml');
-var item = component.create(context);
-
-// Set window title
-item.setProperty('title', 'Helloooooo Brig');
-
-*/
